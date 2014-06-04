82dbba923c9107bb6448bdfdcca3402aca9cb4e2
dsantana25 <dsantana25@outlook.com>
2014-06-04 13:20:33 -0430

    Create diegouno.php

---
+++ diegouno.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang = "es">
+<meta charset = "utf-8"/>
+
+<title> Formulario Impares </title>
+
+<body>
+		<form method = "GET" action = "impares.php" background = "#000000">
+		<b>Numero 1:</b>
+		<input type = "text" name = "num1" autofocus> <br>
+		
+		<input type = "submit" value = "Enviar">	
+		</form>
+</body>	
